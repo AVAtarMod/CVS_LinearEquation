8d64be0e6dd23eda9ea9bfbbb26a673051f81571
dmitry <dmitr-zorin.zorin@yandex.ru>
2022-05-29 14:25:18 +0300

    Added prototypes of feature1

--- LW_Equation/LinearEquation.cs
+++ LW_Equation/LinearEquation.cs
@@ -69,6 +69,26 @@ namespace LW_Equation
         {
             return !first.Equals(second);
         }
+        static public LinearEquation operator *(LinearEquation left, LinearEquation right)
+        {
+            LinearEquation ans = new LinearEquation();
+
+            return ans;
+        }
+        static public LinearEquation operator -(LinearEquation left, LinearEquation right)
+        {
+            LinearEquation ans = new LinearEquation();
+
+            return ans;
+        }
+        static public LinearEquation operator +(LinearEquation left, LinearEquation right)
+        {
+            int size = Math.Max(left.Size, right.Size);
+            LinearEquation ans = new LinearEquation(true, size, 0);
+
+            return ans;
+        }
+
         public float this[int i]
         {
             get { return this.coefficients[i]; }
