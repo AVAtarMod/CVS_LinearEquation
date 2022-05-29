f0f8f5f1d64f99ecd08c8c1ada06a0e5f84e898d
dmitry <dmitr-zorin.zorin@yandex.ru>
2022-05-29 16:28:34 +0300

    Added feature 1, tests accepted

--- LW_Equation/LinearEquation.cs
+++ LW_Equation/LinearEquation.cs
@@ -27,6 +27,20 @@ namespace LW_Equation
             this.coefficients = new List<float>();
             this.coefficients = coefficients;
         }
+        public LinearEquation(bool t, int size)
+        {
+            Random rng = new Random();
+            this.coefficients = new List<float>();
+            for (int i = 0; i < size; i++)
+                coefficients.Add((float)rng.NextDouble() * 100);
+        }
+        public LinearEquation(bool t, int size, float a)
+        {
+            this.coefficients = new List<float>();
+            for (int i = 0; i < size; i++)
+                coefficients.Add(a);
+        }
+
 
         /// <summary>
         /// Суммирует свободный член first с second
@@ -71,27 +85,85 @@ namespace LW_Equation
         }
         static public LinearEquation operator *(LinearEquation left, LinearEquation right)
         {
-            LinearEquation ans = new LinearEquation();
+            int size = Math.Max(left.Size, right.Size);
+            LinearEquation ans = new LinearEquation(true, size, 0);
 
+            for (int i = 1; i <= size; i++)
+            {
+                if ((left.Size - i) < 0 && (right.Size - i) >= 0)
+                {
+                    ans[ans.Size - i] = 0;
+                }
+                else if ((left.Size - i) >= 0 && (right.Size - i) < 0)
+                {
+                    ans[ans.Size - i] = 0;
+                }
+                else
+                {
+                    ans[ans.Size - i] = left[left.Size - i] * right[right.Size - i];
+                }
+            }
+            List<double> c = new List<double>();
+            c = ans.ToList();
+            while (c[0] == 0)
+            {
+                c.RemoveAt(0);
+            }
+            size = c.Count;
+            ans = new LinearEquation(true, size, 0);
+            for (int i = 0; i < ans.Size; i++)
+            {
+                ans[i] = (float)c[i];
+            }
             return ans;
         }
         static public LinearEquation operator -(LinearEquation left, LinearEquation right)
         {
-            LinearEquation ans = new LinearEquation();
-
+            int size = Math.Max(left.Size, right.Size);
+            LinearEquation ans = new LinearEquation(true, size, 0);
+            for (int i = 1; i <= size; i++)
+            {
+                if ((left.Size - i) < 0 && (right.Size - i) >= 0)
+                {
+                    ans[ans.Size - i] = -right[right.Size - i];
+                }
+                else if ((left.Size - i) >= 0 && (right.Size - i) < 0)
+                {
+                    ans[ans.Size - i] = left[left.Size - i];
+                }
+                else
+                {
+                    ans[ans.Size - i] = left[left.Size - i] - right[right.Size - i];
+                }
+            }
             return ans;
         }
         static public LinearEquation operator +(LinearEquation left, LinearEquation right)
         {
             int size = Math.Max(left.Size, right.Size);
             LinearEquation ans = new LinearEquation(true, size, 0);
-
+            for (int i= 1; i <= size; i++)
+            {
+                if ((left.Size - i) < 0 && (right.Size - i) >= 0)
+                {
+                    ans[ans.Size - i] = right[right.Size - i];
+                }
+                else if ((left.Size - i) >= 0 && (right.Size - i) < 0)
+                {
+                    ans[ans.Size - i] = left[left.Size - i];
+                }
+                else
+                {
+                    ans[ans.Size - i] = left[left.Size - i] + right[right.Size - i];
+                }
+            }
             return ans;
         }
 
         public float this[int i]
         {
             get { return this.coefficients[i]; }
+            set { this.coefficients[i] = value; }
         }
         public List<double> ToList()
         {
