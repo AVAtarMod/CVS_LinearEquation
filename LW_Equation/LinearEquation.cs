486315c07eea11e48f11ddd36f56254baeee2677
dmitry <dmitr-zorin.zorin@yandex.ru>
2022-05-31 16:48:42 +0300

    Added feature4

--- LW_Equation/LinearEquation.cs
+++ LW_Equation/LinearEquation.cs
@@ -164,5 +164,17 @@ namespace LW_Equation
             return false;
         }
 
+        public override String ToString()
+        {
+            String ans = "";
+            for (int i = 0; i < this.Size - 1; i++)
+            {
+                ans += this[i].ToString();
+                ans += ",";
+            }
+            ans += this[this.Size - 1].ToString();
+            return ans;
+        }
+
     }
 }
