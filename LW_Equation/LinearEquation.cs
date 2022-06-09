7d853f5ca78499ad9f8d83c05320b14714ac55a8
oldspacce <droidet01@mail.ru>
2022-06-09 19:57:51 +0300

    fix operator plus

--- LW_Equation/LinearEquation.cs
+++ LW_Equation/LinearEquation.cs
@@ -9,7 +9,7 @@ namespace LW_Equation
     public class LinearEquation
     {
         List<double> coefficient;
-        public int Size => coefficients.Count;
+        public int Length { get { return coefficient.Count; } }
 
         /// <summary>
         /// Конструирует уравнение вида coefficients[0]x + ... + coefficients[N-2]y + (aN)z + b = 0
@@ -30,14 +30,36 @@ namespace LW_Equation
             coefficient = list;
         }
 
+        public static Linearequation FillSame(int n, int k)
+        {
+            Linearequation Same = new();
+            for (int i = 0; i < n; i++)
+            {
+                Same.coefficient.Add(k);
+            }
+            return Same;
+        }
         /// <summary>
         /// Суммирует свободный член first с second
         /// </summary>
-        static public LinearEquation operator+ (LinearEquation first, float second)
+        public static LinearEquation operator +(LinearEquation a, LinearEquation b)
         {
-            LinearEquation equation = first;
-            equation.coefficients[0] *= second;
-            return equation;
+            int max = Math.Max(a.Length, b.Length);
+            int min = Math.Min(a.Length, b.Length);
+            LinearEquation count = FillSame(max, 0);
+            for (int i = 0; i < min; i++)
+            {
+                count.coefficient[i] = a.coefficient[i] + b.coefficient[i];
+            }
+            if (max == a.Length)
+                for (int j = min; j < max; j++)
+                {
+                    count.coefficient[j] = a.coefficient[j];
+                }
+            else if (max == b.Length)
+                for (int j = min; j < max; j++)
+                    count.coefficient[j] = b.coefficient[j];
+            return count;
         }
         /// <summary>
         /// Вычитает second из свободного члена first
