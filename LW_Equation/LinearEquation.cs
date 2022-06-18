b9f21bfd7a5c8813499e43c63aa5b63c138e85a0
ulianakh <uliana.kharitonova@gmail.com>
2022-06-18 22:57:44 +0300

    Fix ToDouble

--- LW_Equation/LinearEquation.cs
+++ LW_Equation/LinearEquation.cs
@@ -166,7 +166,12 @@ namespace LW_Equation
         }
         public List<double> ToDouble()
         {
-            return new List<double>();
+            List<double> result = new List<double>();
+            foreach (float c in coefficients)
+            {
+                result.Add((double)c);
+            }
+            return result;
         }
     }
 }
