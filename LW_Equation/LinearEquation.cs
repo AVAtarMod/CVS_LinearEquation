767c6b5ad027639240061204df06abb647ec29c9
Ксения <ksenia.b4ulm@mail.ru>
2022-06-19 14:44:04 +0300

    feat: feature 7

--- LW_Equation/LinearEquation.cs
+++ LW_Equation/LinearEquation.cs
@@ -181,5 +181,13 @@ namespace LW_Equation
             }
             return e;
         }
+        static public LinearEquation operator -(LinearEquation a)
+        {
+            for (int i = 0; i < a.Size; i++)
+            {
+                a.coefficients[i] *= -1;
+            }
+            return a;
+        }
     }
 }
