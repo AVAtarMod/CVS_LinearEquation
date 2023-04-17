b974d98a2a3528a0d8b5af93a134a0a9179d66b8
ksushaVyatsu <stud142911@vyatsu.ru>
2023-04-17 14:54:27 +0300

    feature3 добавлены тесты

--- LW_EquationTest/LinearEquationTest.cs
+++ LW_EquationTest/LinearEquationTest.cs
@@ -207,5 +207,14 @@ namespace LW_EquationTest
             Assert.AreEqual(false, b);
             Assert.AreEqual(true, c);
         }
+        [TestMethod]
+        public void LinearEquationTestGetRezult()
+        {
+            LinearEquation a = new LinearEquation(5, 2);
+            //5x+2=0
+            //x=-2/5
+            float? rez = a.GetRoot(a);
+            Assert.AreEqual(rez, (float)-0.4);
+        }
     }
 }
