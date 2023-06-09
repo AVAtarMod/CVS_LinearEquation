d5017a30d0325b17ffbdc52cef081cae632517e4
Артём Хохолков <79127243325q@gmail.com>
2023-06-10 01:31:34 +0300

    добавлен метод для инициализации уравнения с рандомными числами

--- LW_Equation/LW_Equation.cs
+++ LW_Equation/LW_Equation.cs
@@ -10,6 +10,7 @@ namespace LW_Equation
     {
         List<float> coefficients;
         public int Size => coefficients.Count;
+        static public Random rand = new Random();
 
         /// <summary>
         /// Конструирует уравнение вида aN*x + coefficients[0]y + ... + coefficients[N-2]z + coefficients[N-1] = 0
@@ -217,6 +218,19 @@ namespace LW_Equation
             //добавляем в конце =0
             str += "=0";
             return str;
+        }
+        /// <summary>
+        /// Коструктор, заполнение рандомными числами
+        /// </summary>
+        /// <param name="count">Кол-во коэф. в уравнении</param>
+        /// <returns></returns>
+        public static LinearEquation RandomLinearEq(int count)
+        {
+            List<float> array = new List<float>();
+            for (int i = 0; i < count; i++)
+                array.Add(rand.Next(-100, 100));
+            LinearEquation a = new LinearEquation(array);
+            return a;
         }
     }
 }
